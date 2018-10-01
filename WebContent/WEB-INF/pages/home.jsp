9098a3552218957a60a8a3d648307d9665bba78e
manh hoang <manhhoang@manhs-Mac-mini.local>
2018-10-02 00:01:37 +0700

    postData

--- WebContent/WEB-INF/pages/home.jsp
+++ WebContent/WEB-INF/pages/home.jsp
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
   <head>
+  	<script src="resources/js/jquery.min.js"></script>
+	 <script src="resources/js/bootstrap.min.js"></script>
+	 <script src="resources/js/index.js"></script>
+	 <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+	 <link rel="stylesheet" href=resources/css/font-awesome.min.css>
      <meta charset="UTF-8">
      <title>Simple Web Application</title>
   </head>
@@ -8,7 +13,7 @@
   <body>
   
      <h2>jsp file</h2>
-      
+       
      <ul>
         <li><a href="home">Home</a></li>
         <li><a href="login">Login</a></li>
@@ -16,4 +21,47 @@
      </ul>
       
   </body>
+  <script type="text/javascript">
+ 	 var obj = {
+ 			  "message_id": new Date().getTime(),
+ 			  "timestamp": new Date().getTime(),
+ 			  "farm_id": 1234,
+ 			  "device_id": 345,
+ 			  "node_id": 1234,
+ 			  "sensors_data": [
+ 			    {
+ 			      "sensor_id": 1,
+ 			      "sensor_name": "Do PH",
+ 			      "sensor_value": "7.8"
+ 			    },
+ 			    {
+ 			      "sensor_id": 2,
+ 			      "sensor_name": "DO do am",
+ 			      "sensor_value": "70"
+ 			    },
+ 			    {
+ 			      "sensor_id": 3,
+ 			      "sensor_name": "Do nhiet do",
+ 			      "sensor_value": "37.5"
+ 			    }
+ 			  ]
+ 			}
+		$.ajax({
+			type : 'POST',
+			url : './senser',
+			dataType : 'json',
+			data : JSON.stringify(obj),
+			contentType : 'application/json',
+			async : false,
+			cache : false,
+			processData : false,
+			success : function(res) {
+				console.log("Thông tin của bạn đã được gửi thành công! \n\n Chúng tôi sẽ gửi lại qua email bạn vừa nhập \n\n Vui lòng chờ đợi!")
+			},
+			error : function(e) {
+				console.log('error' + e);
+			}
+		});
+  
+  </script>
 </html>
--- src/com/hust/farmproject/model/SenserData.java
+++ src/com/hust/farmproject/model/SenserData.java
@@ -3,21 +3,21 @@ package com.hust.farmproject.model;
 public class SenserData {
 
 	private Long id;
-	private Long senserId;// phần_cứng_có_Id
-	private String name;
-	private double value;
-	private Long deviceId;
-	private Long farmId;
+	private Long sensor_id;// phần_cứng_có_Id
+	private String sensor_name;
+	private double sensor_value;
+	private Long device_id;
+	private Long farm_id;
 	private Long createDate;
 	
 	public static String createTable() {
 		String myTableName = "CREATE TABLE IF NOT EXISTS SenserData (" 
 				+ "id BIGINT(15) NOT NULL ,"
-				+ "senserId BIGINT(15) NOT NULL ,"
-				+ "name VARCHAR(60) ,"
-				+ "value DOUBLE ,"
-				+ "deviceId BIGINT(15) NOT NULL ," 
-				+ "farmId BIGINT(15) NOT NULL ," 
+				+ "sensor_id BIGINT(15) NOT NULL ,"
+				+ "sensor_name VARCHAR(60) ,"
+				+ "sensor_value DOUBLE ,"
+				+ "device_id BIGINT(15) NOT NULL ," 
+				+ "farm_id BIGINT(15) NOT NULL ," 
 				+ "createDate BIGINT(15) NOT NULL ," 
 				+ "PRIMARY KEY(id))";
 		return myTableName;
@@ -34,46 +34,6 @@ public class SenserData {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getValue() {
-		return value;
-	}
-
-	public void setValue(double value) {
-		this.value = value;
-	}
-
-	public Long getDeviceId() {
-		return deviceId;
-	}
-
-	public void setDeviceId(Long deviceId) {
-		this.deviceId = deviceId;
-	}
-
-	public Long getFarmId() {
-		return farmId;
-	}
-
-	public void setFarmId(Long farmId) {
-		this.farmId = farmId;
-	}
-
-	public Long getSenserId() {
-		return senserId;
-	}
-
-	public void setSenserId(Long senserId) {
-		this.senserId = senserId;
-	}
-
 	public Long getCreateDate() {
 		return createDate;
 	}
@@ -81,4 +41,46 @@ public class SenserData {
 	public void setCreateDate(Long createDate) {
 		this.createDate = createDate;
 	}
+
+	public Long getSensor_id() {
+		return sensor_id;
+	}
+
+	public void setSensor_id(Long sensor_id) {
+		this.sensor_id = sensor_id;
+	}
+
+	public String getSensor_name() {
+		return sensor_name;
+	}
+
+	public void setSensor_name(String sensor_name) {
+		this.sensor_name = sensor_name;
+	}
+
+	public double getSensor_value() {
+		return sensor_value;
+	}
+
+	public void setSensor_value(double sensor_value) {
+		this.sensor_value = sensor_value;
+	}
+
+	public Long getDevice_id() {
+		return device_id;
+	}
+
+	public void setDevice_id(Long device_id) {
+		this.device_id = device_id;
+	}
+
+	public Long getFarm_id() {
+		return farm_id;
+	}
+
+	public void setFarm_id(Long farm_id) {
+		this.farm_id = farm_id;
+	}
+	
+	
 }
